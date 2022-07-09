9c4d9cc8290621a7046ca054dbb6d1c6cc084c39
Ali Eslah <alieslah81@gmail.com>
2022-07-09 20:17:49 +0430

    Start Project With django 3.2 Separate Settings and create .env file for production and development build model manager for user and model build model for profile build model and serializer for rating

---
+++ apps/common/models.py
@@ -0,0 +1,13 @@
+from click import edit
+from django.db import models
+import uuid
+
+
+class TImeStampedUUIDModel(models.Model):
+    pkid = models.BigAutoField(primary_key=True, editable=False)
+    id = models.UUIDField(default=uuid.uuid4, editable=False, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
